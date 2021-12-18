b1dc0be6ea57368801166ccd9fb4c784ce31c107
Eric Yi <erickanginyi@gmail.com>
2021-12-18 02:59:09 -0500

    Add initial routes, add initial text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./Stylings/App.css"
+
+// Import "Pages"
+import Page1 from "./Components/Page1";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/page1" element={<Page1/>}/>
+				<Route path="/" element={
+					<div>
+						<h1>Hello World!</h1>
+						<p>My name is Eric and I'm trying to learn more about web development. Welcome to a website inspired by <a href="https://jenniferdewalt.com/" target="_blank">Jennifer Dewalt's</a> 180 Websites in 180 Days project.</p>
+						<a href="/page1">Page 1</a><br/>
+						<a href="/">Homepage</a>
+					</div>
+				}/>
+			</Routes>
+		</Router>   
+	);
 }
 
 export default App;
