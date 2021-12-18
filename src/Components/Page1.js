c06586d0fd6051b4f9625f265102cd0618db56d5
Eric Yi <erickanginyi@gmail.com>
2021-12-18 02:56:52 -0500

    Create folders to hold individual pieces

---
+++ src/Components/Page1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Page1() {
+    return (
+        <a href="https://www.instagram.com/__.henri.__/" target="_blank">Henri</a>
+    );
+}
+
+export default Page1;
