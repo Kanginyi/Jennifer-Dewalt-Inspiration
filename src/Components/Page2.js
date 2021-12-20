55ce9348be6e7ab58799c437fffca5dc086468e8
Eric Yi <erickanginyi@gmail.com>
2021-12-20 15:19:07 -0500

    Create file, populate with initial idea

---
+++ src/Components/Page2.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import "../Stylings/Page2.css"
+
+function Page2() {
+    return (
+        <>
+        <div id="page2">
+            <header>
+                <h1>Flexbox Practice and Examples</h1>
+            </header>
+            <br/><br/><br/>
+
+            <div id="flexbox-parent-container">
+                <div className="flexbox-divs">
+                    Jasdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+                
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+                
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+                
+                <div className="flexbox-divs">
+                    asdf
+                </div>
+            </div>
+        </div>
+
+        <footer id="flexbox-footer">
+            <p>MORE RESOURCES:</p>
+            <a
+                href="https://css-tricks.com/snippets/css/a-guide-to-flexbox/"
+                title="Guide to Flexbox"
+                target="_blank"
+            >
+                CSS Tricks: A Guide to Flexbox
+            </a><br/>
+            <a
+                href="https://www.youtube.com/playlist?list=PL4-IK0AVhVjMSb9c06AjRlTpvxL3otpUd"
+                title="Kevin Powell's Flexbox Tutorial Playlist"
+                target="_blank"
+            >
+                Kevin Powell's Flexbox Tutorial Playlist
+            </a><br/>
+            <a
+                href="https://flexboxfroggy.com/"
+                title="Flexbox Froggy"
+                target="_blank"
+            >
+                Flexbox Froggy: A Game for Learning Flexbox
+            </a>
+        </footer>
+        </>
+    );
+}
+
+export default Page2;
