bd31c004f16636f60836cbda7c3bf27134055ce7
Eric Yi <erickanginyi@gmail.com>
2021-12-20 10:29:28 -0500

    Import react-window elements and implement rough draft of it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { FixedSizeList as List } from "react-window";
 import "./App.css"
 
 // Import "Pages"
 import Page1 from "./Components/Page1";
 
 function App() {
+  
+    // If index === 0, render Homepage and make sure that it doesn't render anythingelse, just bings into homepage
+    // Anything else, increment by 1 and then go from there
+
+    const Row = ({ index, style }) => (
+        // <div style={style}> Row {index}</div>
+        <a style={style} href={`/page${index}`}>Page {index}</a>
+    );
+
+    console.log(Row);
+
 	return (
 		<Router>
 			<Routes>
@@ -15,6 +28,17 @@ function App() {
 						<p>My name is Eric and I'm trying to learn more about web development. Welcome to a website inspired by <a href="https://jenniferdewalt.com/" target="_blank">Jennifer Dewalt's</a> 180 Websites in 180 Days project.</p>
 						<a href="/page1">Page 1</a><br/>
 						<a href="/">Homepage</a>
+
+                        <div id="homepage-list">
+                            <List
+                                itemCount={100}
+                                itemSize={35}
+                                height={250}
+                                width={450}
+                            >
+                                {Row}
+                            </List>
+                        </div>
 					</div>
 				}/>
 			</Routes>
