0a4473483281f25c778170b75e1c8d6387eaa394
Eric Yi <erickanginyi@gmail.com>
2021-12-19 21:13:44 -0500

    Add id to homepage div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./Stylings/App.css"
+import "./App.css"
 
 // Import "Pages"
 import Page1 from "./Components/Page1";
@@ -10,7 +10,7 @@ function App() {
 			<Routes>
 				<Route path="/page1" element={<Page1/>}/>
 				<Route path="/" element={
-					<div>
+					<div id="homepage">
 						<h1>Hello World!</h1>
 						<p>My name is Eric and I'm trying to learn more about web development. Welcome to a website inspired by <a href="https://jenniferdewalt.com/" target="_blank">Jennifer Dewalt's</a> 180 Websites in 180 Days project.</p>
 						<a href="/page1">Page 1</a><br/>
