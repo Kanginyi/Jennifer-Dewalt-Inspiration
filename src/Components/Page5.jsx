8dd97d0196e38eb6b45c734f2a6f9356c7374a8e
Eric Yi <erickanginyi@gmail.com>
2021-12-27 03:01:35 -0500

    Add divs to better style regex info

--- src/Components/Page5.jsx
+++ src/Components/Page5.jsx
@@ -18,171 +18,185 @@ function Page5() {
             <p><b>/RegEx/flags</b></p>
             <br/><br/>
 
-            <div id="regex-divs-container">
-                <div>
-                    <p>Character Classes</p>
-                    <ul>
-                        <li>
-                            <span>.</span>
-                            &nbsp;Any character except new line
-                        </li>
-                        <li>
-                            <span>\w \W</span>
-                            &nbsp;Word (Alphanumeric characters & underscore), Opposite of Word
-                        </li>
-                        <li>
-                            <span>\d \D</span>
-                            &nbsp;Digit (0~9), Opposite of Digit
-                        </li>
-                        <li>
-                            <span>\s \S</span>
-                            &nbsp;Whitespace (Spaces, tabs, linebreaks), Opposite of Whitespace
-                        </li>
-                    </ul>
+            <section id="regex-divs-container">
+                <div className="regex-divs">
+                    <div className="regex-cards-content">
+                        <p>Character Classes</p>
+                        <ul>
+                            <li>
+                                <span>.</span>
+                                &nbsp;Any character except new line
+                            </li>
+                            <li>
+                                <span>\w \W</span>
+                                &nbsp;Word (Alphanumeric characters & underscore), Opposite of Word
+                            </li>
+                            <li>
+                                <span>\d \D</span>
+                                &nbsp;Digit (0~9), Opposite of Digit
+                            </li>
+                            <li>
+                                <span>\s \S</span>
+                                &nbsp;Whitespace (Spaces, tabs, linebreaks), Opposite of Whitespace
+                            </li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div>
-                    <p>Brackets</p>
-                    <ul>
-                        <li>
-                            <span>[abc]</span>
-                            &nbsp;Any single character of a, b, or c
-                        </li>
-                        <li>
-                            <span>[^xyz]</span>
-                            &nbsp;Any single character EXCEPT x, y, or z
-                        </li>
-                        <li>
-                            <span>[a-z]</span>
-                            &nbsp;Any single character in the range of a~z
-                        </li>
-                        <li>
-                            <span>[^0~9]</span>
-                            &nbsp;Any single character EXCEPT 0~9
-                        </li>
-                        <li>
-                            <span>[a-f0-5]</span>
-                            &nbsp;Any single character in the range of a~f or 0~5
-                        </li>
-                    </ul>
+                <div className="regex-divs">
+                    <div className="regex-cards-content">
+                        <p>Brackets</p>
+                        <ul>
+                            <li>
+                                <span>[abc]</span>
+                                &nbsp;Any single character of a, b, or c
+                            </li>
+                            <li>
+                                <span>[^xyz]</span>
+                                &nbsp;Any single character EXCEPT x, y, or z
+                            </li>
+                            <li>
+                                <span>[a-z]</span>
+                                &nbsp;Any single character in the range of a~z
+                            </li>
+                            <li>
+                                <span>[^0~9]</span>
+                                &nbsp;Any single character EXCEPT 0~9
+                            </li>
+                            <li>
+                                <span>[a-f0-5]</span>
+                                &nbsp;Any single character in the range of a~f or 0~5
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 
-                <div>
-                    <p>Alternation & Quantifiers</p>
-                    <ul>
-                        <li>
-                            <span>a|z</span>
-                            &nbsp;Alternate: Match either a or z
-                        </li>
-                        <li>
-                            <span>a?</span>
-                            &nbsp;Zero or One occurrences of a
-                        </li>
-                        <li>
-                            <span>b*</span>
-                            &nbsp;Zero or more occurrences of b
-                        </li>
-                        <li>
-                            <span>c+</span>
-                            &nbsp;One or more occurrences of c
-                        </li>
-                        <li>
-                            <span>x{`{n}`}</span>
-                            &nbsp;Exactly n occurrences of x
-                        </li>
-                        <li>
-                            <span>y{`{n, }`}</span>
-                            &nbsp;n or more occurrences of y
-                        </li>
-                        <li>
-                            <span>z{`{n, o}`}</span>
-                            &nbsp;Between n or o occurrences of z
-                        </li>
-                    </ul>
+                <div className="regex-divs">
+                    <div className="regex-cards-content">
+                        <p>Alternation & Quantifiers</p>
+                        <ul>
+                            <li>
+                                <span>a|z</span>
+                                &nbsp;Alternate: Match either a or z
+                            </li>
+                            <li>
+                                <span>a?</span>
+                                &nbsp;Zero or One occurrences of a
+                            </li>
+                            <li>
+                                <span>b*</span>
+                                &nbsp;Zero or more occurrences of b
+                            </li>
+                            <li>
+                                <span>c+</span>
+                                &nbsp;One or more occurrences of c
+                            </li>
+                            <li>
+                                <span>x{`{n}`}</span>
+                                &nbsp;Exactly n occurrences of x
+                            </li>
+                            <li>
+                                <span>y{`{n, }`}</span>
+                                &nbsp;n or more occurrences of y
+                            </li>
+                            <li>
+                                <span>z{`{n, o}`}</span>
+                                &nbsp;Between n or o occurrences of z
+                            </li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div>
-                    <p>Grouping</p>
-                    <ul>
-                        <li>
-                            <span>(abc)</span>
-                            &nbsp;Capture Group: Capture and match what's inside of the parentheses
-                        </li>
-                        <li>
-                            <span>(?:xyz)</span>
-                            &nbsp;Group what's inside of the parentheses without creating a Capture Group
-                        </li>
-                        <li>
-                            <span>(?=abc)</span>
-                            &nbsp;Positive Lookahead: Match something followed by abc
-                        </li>
-                        <li>
-                            <span>(?!xyz)</span>
-                            &nbsp;Negative Lookahead: Match something not followed by xyz
-                        </li>
-                        <li>
-                            <span>(?{`<=`}abc)</span>
-                            &nbsp;Positive Lookbehind: Match something preceded by abc
-                        </li>
-                        <li>
-                            <span>(?{`<!`}xyz)</span>
-                            &nbsp;Negative Lookbehind: Match something not preceded by xyz
-                        </li>
-                    </ul>
+                <div className="regex-divs">
+                    <div className="regex-cards-content">
+                        <p>Grouping</p>
+                        <ul>
+                            <li>
+                                <span>(abc)</span>
+                                &nbsp;Capture Group: Capture and match what's inside of the parentheses
+                            </li>
+                            <li>
+                                <span>(?:xyz)</span>
+                                &nbsp;Group what's inside of the parentheses without creating a Capture Group
+                            </li>
+                            <li>
+                                <span>(?=abc)</span>
+                                &nbsp;Positive Lookahead: Match something followed by abc
+                            </li>
+                            <li>
+                                <span>(?!xyz)</span>
+                                &nbsp;Negative Lookahead: Match something not followed by xyz
+                            </li>
+                            <li>
+                                <span>(?{`<=`}abc)</span>
+                                &nbsp;Positive Lookbehind: Match something preceded by abc
+                            </li>
+                            <li>
+                                <span>(?{`<!`}xyz)</span>
+                                &nbsp;Negative Lookbehind: Match something not preceded by xyz
+                            </li>
+                        </ul>
+                    </div>
                 </div> 
 
-                <div>
-                    <p>Escaped Characters</p>
-                    <ul>
-                        <li>
-                            <span>\. \* \\ </span>
-                            &nbsp;Escaped Characters: Make any character literal. 
-                            Most commonly used to escape special characters.
-                        </li>
-                        <li>
-                            <span>\t</span>
-                            &nbsp;Tab
-                        </li>
-                        <li>
-                            <span>\n</span>
-                            &nbsp;New line or Line break
-                        </li>
-                        <li>
-                            <span>\r</span>
-                            &nbsp;Carriage Return
-                        </li>
-                    </ul>
+                <div className="regex-divs">
+                    <div className="regex-cards-content">
+                        <p>Escaped Characters</p>
+                        <ul>
+                            <li>
+                                <span>\. \* \\ </span>
+                                &nbsp;Escaped Characters: Make any character literal. 
+                                Most commonly used to escape special characters.
+                            </li>
+                            <li>
+                                <span>\t</span>
+                                &nbsp;Tab
+                            </li>
+                            <li>
+                                <span>\n</span>
+                                &nbsp;New line or Line break
+                            </li>
+                            <li>
+                                <span>\r</span>
+                                &nbsp;Carriage Return
+                            </li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div>
-                    <p>Anchors</p>
-                    <ul>
-                        <li>
-                            <span>\b \B</span>
-                            &nbsp;Word Boundary, Opposite of Word Boundary
-                        </li>
-                        <li>
-                            <span>\A</span>
-                            &nbsp;Start of string
-                        </li>
-                        <li>
-                            <span>^a</span>
-                            &nbsp;Match string that starts with a
-                        </li>
-                        <li>
-                            <span>\Z</span>
-                            &nbsp;End of string
-                        </li>
-                        <li>
-                            <span>z$</span>
-                            &nbsp;Match string that ends with z
-                        </li>
-                        <li>
-                            <span>^abc$</span>
-                            &nbsp;Match exactly abc
-                        </li>
-                    </ul>
+                <div className="regex-divs">
+                    <div className="regex-cards-content">
+                        <p>Anchors</p>
+                        <ul>
+                            <li>
+                                <span>\b \B</span>
+                                &nbsp;Word Boundary, Opposite of Word Boundary
+                            </li>
+                            <li>
+                                <span>\A</span>
+                                &nbsp;Start of string
+                            </li>
+                            <li>
+                                <span>^a</span>
+                                &nbsp;Match string that starts with a
+                            </li>
+                            <li>
+                                <span>\Z</span>
+                                &nbsp;End of string
+                            </li>
+                            <li>
+                                <span>z$</span>
+                                &nbsp;Match string that ends with z
+                            </li>
+                            <li>
+                                <span>^abc$</span>
+                                &nbsp;Match exactly abc
+                            </li>
+                        </ul>
+                    </div>
                 </div>
+
+                </section>
 
                 <div>
                     <p>Flags</p>
@@ -219,8 +233,6 @@ function Page5() {
                         </li>
                     </ul>
                 </div> 
-
-            </div>
 
             
             <br/><br/>
