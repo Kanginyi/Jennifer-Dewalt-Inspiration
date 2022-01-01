d3253bdd3b5e8f626014cd27df6625593eeaf733
Eric Yi <erickanginyi@gmail.com>
2021-12-31 23:31:57 -0500

    Create file, add placeholder text

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../Stylings/Navbar.css";
+
+function Navbar() {
+    return (
+        <div id="asdf">
+            biansd
+        </div>
+    );
+}
+
+export default Navbar;
